fe8c7553ad06a974ffd15fb665f4c6cde83bb1b1
altakori <126345795+altakori@users.noreply.github.com>
2024-06-02 19:07:27 +0900

    Update and rename DataAnalize.md to DataAnalize.py

--- Opensource/DataAnalize.py
+++ Opensource/DataAnalize.py
@@ -4,4 +4,3 @@ top_corr_features = corrmat.index
 plt.figure(figsize=(20,20))
 g=sn.heatmap(corrmat, annot=True, cmap="PuBu")
 plt.show()
-![image](https://github.com/DataScience-team8/end-to-end-data-science-project/assets/126345795/a062d779-bd27-42cd-8cd6-162b02c7a5c8)
